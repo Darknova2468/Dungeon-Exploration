77ce34fb07e779c76c1a7b4471b248a7926c5c74
Darknova2468 <112370026+Darknova2468@users.noreply.github.com>
2023-11-21 09:17:54 -0600

    cleaned up and improved readability of code

--- dungeons.js
+++ dungeons.js
@@ -1,69 +1,31 @@
 class DungeonMap {
   constructor(_numberOfRooms){
+    //builds room nodes;
     this.dungeon = [new Room(15)];
     for(let i=1; i<_numberOfRooms-1; i++){
       this.dungeon.push(new Room(20));
     }
     this.dungeon.push(new Room(30));
+
+    //adds procedural distances
     for(let i=1; i<_numberOfRooms; i++){
-      let pathChance = random();
-      if(pathChance < 0.8 || i+2 > _numberOfRooms){
-        if(pathChance < 0.4 || i+2 > _numberOfRooms){
-          this.dungeon[i-1].addConnection([i], this.dungeon);
-        } 
-        else {
-          this.dungeon[i-1].addConnection([i+1], this.dungeon);
-        }
-        
-      } 
-      else {
-        this.dungeon[i-1].addConnection([i, i+1], this.dungeon);
-      }
+      this.dungeon[i-1].addConnection(1+(random() > 0.8 || i+2 > _numberOfRooms), i, _numberOfRooms, this.dungeon, i === _numberOfRooms-1);
     }
-    this.dungeon[0].pos = [0,0];
-    let dist1 = 45; 
-    let dist2 = 45; 
-    let dist3 = 50;
-    if(this.dungeon[0].connections.length === 1){
-      if(this.dungeon[0].connections[0][0] === 1){
-        dist1 = this.dungeon[0].connections[0][1];
-      }
-      else{
-        dist2 = this.dungeon[0].connections[0][1];
-      }
-    }
-    else {
-      dist1 = this.dungeon[0].connections[0][1];
-      dist2 = this.dungeon[0].connections[1][1];
-    }
-    if(this.dungeon[1][0] === 2){
-      dist3 = this.dungeon[1].connections[0][1];
-    }
-    let theta = acos((dist1*dist1+dist2*dist2-dist3*dist3)/(2*dist1*dist2))*0.5;
+
+    //defines starting triangle
+    let dist1 = this.dungeon[0].connections[0][1];
+    let dist2 = this.dungeon[0].connections[1][1];
+    let dist3 = this.dungeon[1].connections[0][1];
+    let theta = 0.5*cosineLaw(dist1, dist2, dist3);
     this.dungeon[1].pos = [Math.abs(cos(theta)*dist1), sin(theta)*dist1];
     this.dungeon[2].pos = [Math.abs(cos(theta)*dist2), -sin(theta)*dist2];
+
+    //generates the rest of the tree
     for(let i=3; i<this.dungeon.length; i++){
-      dist1 = 50;
-      dist2 = 50;
-      if(this.dungeon[i-1].connections[0][0] === i){
-        dist1 = this.dungeon[i-1].connections[0][1];
-      }
-      else if(i === this.dungeon.length-1){
-        dist1 = 60;
-      }
-      if(this.dungeon[i-2].connections.length === 1 && this.dungeon[i-2].connections[0][0] === i){
-        dist2 = this.dungeon[i-2].connections[0][1];
-      }
-      else if(this.dungeon[i-2].connections.length === 2){
-        if(this.dungeon[i-2].connections[1][0] === i){
-          dist2 = this.dungeon[i-2].connections[1][1];
-        } 
-      }
-      else if(i === this.dungeon.length-1){
-        dist2 = 60;
-      }
+      dist1 = this.dungeon[i-1].connections[0][1];
+      dist2 = this.dungeon[i-2].connections[1][1];
       let angle1 = Math.abs(atan((this.dungeon[i-1].pos[0]-this.dungeon[i-2].pos[0])/(this.dungeon[i-1].pos[1]-this.dungeon[i-2].pos[1])));
-      theta = angle1 + acos((dist1*dist1+dist3*dist3-dist2*dist2)/(2*dist1*dist3));
+      theta = angle1 + cosineLaw(dist1, dist3, dist2);
       this.dungeon[i].pos = [this.dungeon[i-1].pos[0]+sin(theta)*dist1, this.dungeon[i-1].pos[1]+cos(theta)*dist1];
     }
   }
@@ -73,17 +35,35 @@ class Room {
   constructor(_radius){
     this.radius = _radius;
     this.connections = [];
-    this.pos = undefined;
+    this.pos = [0, 0];
   }
-  addConnection(_connections, dungeon){
-    _connections.forEach(connection => {
-      let stairOrMaze = random();
-      if(stairOrMaze > 0.7){
-        this.connections.push([connection, 10+this.radius+dungeon[connection].radius]);
+  addConnection(numberOfConnections, index, numberOfRooms, dungeon, check){
+    //pushes connections to node
+    let distance = random() < 0.7 ? 10:Math.floor(random(20, 30));
+    if(check){
+      this.connections.push([index, dungeon[index].radius+this.radius+distance, 1+(distance>10)]);
+    }
+    else {
+      if(numberOfConnections === 1){      
+        if(random() < 0.5 && index+1 < numberOfRooms){
+          this.connections.push([index, dungeon[index].radius+this.radius+distance, 1+(distance>10)]);
+          this.connections.push([index+1, dungeon[index].radius+this.radius+10, 0]);
+        }
+        else {
+          this.connections.push([index, dungeon[index].radius+this.radius+10, 0]);
+          this.connections.push([index+1, dungeon[index].radius+this.radius+distance, 1+(distance>10)]);
+        }
       }
       else {
-        this.connections.push([connection, Math.floor(random(20, 30))+this.radius+dungeon[connection].radius]);
+        this.connections.push([index, dungeon[index].radius+this.radius+distance, 1+(distance>10)]);
+        distance = random() < 0.7 ? 10:Math.floor(random(20, 30));
+        this.connections.push([index+1, dungeon[index].radius+this.radius+distance, 1+(distance>10)]);
       }
-    });
+    }
   }
+}
+
+//finds an angle given three side lengths
+function cosineLaw(a, b, c){
+  return acos((a*a+b*b-c*c)/(2*a*b));
 }
