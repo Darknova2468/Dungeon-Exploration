cbead5d30d0c9a60bdd5871dab2b35660c29d551
Darknova2468 <112370026+Darknova2468@users.noreply.github.com>
2023-11-24 22:39:14 -0600

    added that player overlay and scene background

--- sketch.js
+++ sketch.js
@@ -8,21 +8,30 @@
 
 let myDungeon;
 let minimap;
-let me;
+let player;
+let tileSet;
+let myBackground;
+
+function preload() {
+  tileSet = new TileSet("CaveTiles.png", [16, 16]);
+}
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(600, 300);
   textAlign(CENTER);
   noSmooth();
   myDungeon = new DungeonMap(5, 0.3);
   minimap = new MiniMap(30, myDungeon.minimap);
-  me = new player(myDungeon.playerPos, 0, 0, 5, myDungeon.minimap);
+  player = new Player(myDungeon.playerPos, 0, 0, 5, myDungeon.minimap);
+  myBackground = tileSet.generateImage(myDungeon.minimap);
 }
 
 function draw() {
   background(64);
-  me.move([keyIsDown(68)-keyIsDown(65) ,keyIsDown(83)-keyIsDown(87)], 1/frameRate());
-  let img = minimap.generateImage(me.pos);
+  image(myBackground, (-player.pos[0]+9.375)*32, (-player.pos[1]+4.75)*32, myBackground.width*2, myBackground.height*2);
+  player.move([keyIsDown(68)-keyIsDown(65) ,keyIsDown(83)-keyIsDown(87)], 1/frameRate());
+  let img = minimap.generateImage(player.pos);
+  circle(width/2, height/2, width/24);
   image(img, width-height*5/20, height*1/20, height/5, height/5);
   text("fps: " + Math.floor(frameRate()), width-height*3/20, height*6/20);
 }
