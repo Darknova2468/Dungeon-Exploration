2a5baaf7a7f1cc6ae155bdb1b92249b033b19e0c
Darknova2468 <112370026+Darknova2468@users.noreply.github.com>
2023-11-24 22:38:29 -0600

    modified the collisions of the player map to account for the size of the player sprite

--- entities.js
+++ entities.js
@@ -1,4 +1,4 @@
-class player {
+class Player {
   constructor(_pos, _health, _defence, _speed, _collisionMap){
     this.pos = _pos;
     this.health = _health;
@@ -9,8 +9,10 @@ class player {
   move(direction, time){
     let [i, j] = direction;
     let distance = sqrt(i*i + j*j)!== 0 ? time*this.speed/sqrt(i*i + j*j) : 0;
-    if(this.collisionMap[Math.floor(this.pos[1] + j*distance)][Math.floor(this.pos[0] + i*distance)] !== 0){
+    if(this.collisionMap[Math.floor(this.pos[1])][Math.floor(this.pos[0] + direction[0]*0.5 + i*distance)  ] !== 0){
       this.pos[0] += i*distance;
+    }
+    if(this.collisionMap[Math.floor(this.pos[1]+direction[1]*0.5 + j*distance)][Math.floor(this.pos[0])] !== 0){
       this.pos[1] += j*distance;
     }
   }
