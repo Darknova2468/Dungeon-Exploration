22ce6b47b203c1f820ec77496f6d1ff3508f17b3
unevenlogic <robertdotyang@gmail.com>
2023-12-15 08:39:07 -0600

    Fixed incorrect sword placement
    - Sword now extends to the correct range
    - Sword now has a minimum range, not intersecting with player

--- entities/weapons.js
+++ entities/weapons.js
@@ -25,6 +25,7 @@ class SweepWeapon extends MeleeWeapon {
   constructor(wielder, damage, range, delay, sweepRange) {
     super(wielder, damage, range, delay);
     this.sweepRange = sweepRange;
+    this.holdRange = 0.3;
   }
 
   attack(enemies, direction, time) {
@@ -34,7 +35,7 @@ class SweepWeapon extends MeleeWeapon {
     for(let enemy of enemies) {
       let distance = dist(enemy.pos[0], enemy.pos[1], this.wielder.pos[0], this.wielder.pos[1]);
       let targetVector = [enemy.pos[0] - this.wielder.pos[0], enemy.pos[1] - this.wielder.pos[1]];
-      if(distance <= this.range && dotProduct(scaleVector(direction), scaleVector(targetVector)) > this.sweepRange) {
+      if(this.holdRange < distance && distance <= this.range && dotProduct(scaleVector(direction), scaleVector(targetVector)) > this.sweepRange) {
         enemy.damage(this.damage, "Slashing");
         if(WEAPONDEBUG) {
           console.log("[Weapons] An enemy was hit!");
@@ -46,12 +47,14 @@ class SweepWeapon extends MeleeWeapon {
   }
 
   display(screenCenter, screenSize) {
-    let heldPos = dungeonToScreenPos(this.wielder.pos, screenCenter, screenSize);
-    let dungeonWeaponDisplacement = scaleVector([mouseX - width/2, mouseY - height/2], this.sweepRange);
-    let tipPos = dungeonToScreenPos([this.wielder.pos[0] + dungeonWeaponDisplacement[0], this.wielder.pos[1] + dungeonWeaponDisplacement[1]], screenCenter, screenSize);
+    // let basePos = dungeonToScreenPos(this.wielder.pos, screenCenter, screenSize);
+    let heldDisplacement = scaleVector([mouseX - width/2, mouseY - height/2], this.holdRange);
+    let tipDisplacement = scaleVector([mouseX - width/2, mouseY - height/2], this.range);
+    let heldPos = dungeonToScreenPos([this.wielder.pos[0] + heldDisplacement[0], this.wielder.pos[1] + heldDisplacement[1]], screenCenter, screenSize);
+    let tipPos = dungeonToScreenPos([this.wielder.pos[0] + tipDisplacement[0], this.wielder.pos[1] + tipDisplacement[1]], screenCenter, screenSize);
     stroke(10);
     line(heldPos[0], heldPos[1], tipPos[0], tipPos[1]);
-    console.log(heldPos, tipPos, dungeonWeaponDisplacement);
+    // console.log(heldPos, tipPos, dungeonWeaponDisplacement);
   }
 }
 
