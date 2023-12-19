90d5b737793b977aa51c26c809c861cb00418020
unevenlogic <robertdotyang@gmail.com>
2023-12-19 11:05:19 -0600

    Started progress on thrusting weapons
    ThrustWeapons in progress

--- entities/weapons.js
+++ entities/weapons.js
@@ -141,6 +141,86 @@ class BattleAxe extends SweepWeapon {
   }
 }
 
+class ThrustWeapon extends Weapon {
+  constructor(wielder, damage, minRange, maxRange, range, cooldown, thrustTime, pierceFactor) {
+    super(wielder, damage, range, cooldown);
+    this.holdRange = 0.3;
+    this.minRange = minRange;
+    this.maxRange = maxRange;
+    this.thrustTimer = 0;
+    this.thrustTime = thrustTime;
+    this.pointingAngle = 0;
+    this.pierceFactor = pierceFactor;
+  }
+
+  thrust(enemies, direction, time) {
+    if(WEAPONDEBUG) {
+      console.log("[Weapons] Thrusted.");
+    }
+    let hitEnemies = [];
+    for(let enemy of enemies) {
+      // let distance = dist(enemy.pos[0], enemy.pos[1], this.wielder.pos[0], this.wielder.pos[1]);
+      // let targetVector = [enemy.pos[0] - this.wielder.pos[0], enemy.pos[1] - this.wielder.pos[1]];
+      // if(this.minRange < distance && distance <= this.range && dotProduct(scaleVector(direction), scaleVector(targetVector)) > this.sweepRange) {
+      //   hitEnemies.push(enemy);
+      // }
+    }
+    if(hitEnemies.length === 0) {
+      this.pointingAngle = getAngle(mouseY - height/2, mouseX - width/2);
+      this.thrustTimer = millis();
+      return this.cooldown / 2 + millis();
+    }
+    let hitFirstEnemy = false;
+    for(let enemy of hitEnemies) {
+      if(hitFirstEnemy && this.pierceFactor > 0) {
+        enemy.damage(this.damage / Math.pow(hitEnemies.length, 1 - this.pierceFactor), "Piercing");
+      }
+      else if(!hitFirstEnemy) {
+        enemy.damage(this.damage, "Piercing");
+        hitFirstEnemy = true;
+      }
+      if(WEAPONDEBUG) {
+        console.log("[Weapons] An enemy was hit!");
+      }
+    }
+    this.pointingAngle = getAngle(mouseY - height/2, mouseX - width/2);
+    this.thrustTimer = millis();
+    return this.cooldown + millis();
+  }
+
+  attack(enemies, direction, time, isRolling) {
+    if(mouseIsPressed && millis() > this.attackTimer && !isRolling) {
+      this.attackTimer = this.thrust(enemies, direction, time);
+    }
+  }
+
+  display(screenCenter, screenSize) {
+    let directionVector;
+    if(millis() - this.thrustTimer >= this.thrustTime) {
+      // directionVector = [mouseX - width/2, mouseY - height/2];
+      if([1,4,6].includes(player.animationNum[0])) {
+        directionVector = [1, 1];
+      }
+      else {
+        directionVector = [-1, 1];
+      }
+    }
+    else {
+      // directionVector = [mouseX - width/2, mouseY - height/2];
+      directionVector = [cos(this.pointingAngle), sin(this.pointingAngle)];
+    }
+    // let basePos = dungeonToScreenPos(this.wielder.pos, screenCenter, screenSize);
+    let heldDisplacement = scaleVector(directionVector, this.holdRange);
+    let tipDisplacement = scaleVector(directionVector, this.holdRange * 3);
+    let heldPos = dungeonToScreenPos([this.wielder.pos[0] + heldDisplacement[0], this.wielder.pos[1] + heldDisplacement[1]], screenCenter, screenSize);
+    let tipPos = dungeonToScreenPos([this.wielder.pos[0] + tipDisplacement[0], this.wielder.pos[1] + tipDisplacement[1]], screenCenter, screenSize);
+    stroke(10);
+    line(heldPos[0], heldPos[1], tipPos[0], tipPos[1]);
+    // console.log(heldPos, tipPos, dungeonWeaponDisplacement);
+    this.projectiles.forEach((x) => x.display(screenCenter, screenSize));
+  }
+}
+
 // class Hyperion extends SweepWeapon {
 //   constructor(wielder) {
 //     super(wielder, 5, 0, 5, 150, Math.PI - 0.01, 100, 1);
