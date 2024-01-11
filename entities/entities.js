ecd03e6ac5efcb6c25dab48eb00d357fc8cb0dd8
Darknova2468 <112370026+Darknova2468@users.noreply.github.com>
2024-01-11 11:01:43 -0600

    balancing and resolved merge conflicts

--- entities/entities.js
+++ entities/entities.js
@@ -242,7 +242,7 @@ class Enemy extends Entity {
 
 class Player extends Entity {
   constructor(_pos, _collisionMap){
-    super(_pos, 10, 0, 3.5, _collisionMap, textures.playerTileSet);
+    super(_pos, 10, 5, 3.5, _collisionMap, textures.playerTileSet);
     this.rollSpeed = 5;
     this.defaultSpeed = 3.5;
     this.movementDirection = [0, 0]; // Unrelated to texturing
